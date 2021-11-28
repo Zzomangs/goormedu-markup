74051f782192158501577adae36fe0c88a3c7254
over0212 <liberty0212@naver.com>
2021-11-28 15:15:53 +0900

    2021-11-28 수정

---
+++ js/class_home.js
@@ -0,0 +1,14 @@
+const nav_atags = document.querySelectorAll('.nav li a');
+
+nav_atags[0].onclick = () => {
+    location.href = 'class_home.html'; 
+}
+nav_atags[1].onclick = () => {
+    location.href = 'class_home.html'; 
+}
+nav_atags[2].onclick = () => {
+    location.href = 'class_home.html'; 
+}
+nav_atags[3].onclick = () => {
+    location.href = 'class_home.html'; 
+}
