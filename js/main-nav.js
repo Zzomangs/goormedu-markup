bda59e1db5ba813ea1fdd4ef211fd9d7dec3d204
Zzomangs <rkddPfud@gmail.com>
2021-12-05 20:45:10 +0900

    제이쿼리를 이용하여 전체메뉴 토글링, 리뷰화면에 질문서치 구현, 헤더 네비부분 버그 수정

---
+++ js/main-nav.js
@@ -0,0 +1,24 @@
+$(function (){
+	$("#btn_toggle").click(function (){
+  	$("#Toggle").toggle();
+  });
+});
+
+$(function (){
+	$("#btn_Toggle").click(function (){
+  	$("#toggle").toggle();
+  });
+});
+
+$(function (){
+	$("#btn_Toggle2").click(function (){
+  	$("#toggle2").toggle();
+  });
+});
+
+$(function (){
+	$("#btn_Toggle3").click(function (){
+  	$("#toggle3").toggle();
+  });
+});
+
