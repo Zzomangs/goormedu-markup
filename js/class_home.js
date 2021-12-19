a239de8ae70997ccf04ffdf633a170567cc1ff88
Zzomangs <nini6923@naver.com>
2021-12-19 20:19:12 +0900

    21.12.19 제이쿼리수정

--- js/class_home.js
+++ js/class_home.js
@@ -1,14 +1,32 @@
 const nav_atags = document.querySelectorAll('.nav li a');
 
-nav_atags[0].onclick = () => {
-    location.href = 'class_home.html'; 
-}
-nav_atags[1].onclick = () => {
-    location.href = 'class_home.html'; 
-}
-nav_atags[2].onclick = () => {
-    location.href = 'class_home.html'; 
-}
-nav_atags[3].onclick = () => {
-    location.href = 'class_home.html'; 
-}
+// nav_atags[0].onclick = () => {
+//     location.href = 'class_home.html'; 
+// }
+// nav_atags[1].onclick = () => {
+//     location.href = 'class_home.html'; 
+// }
+// nav_atags[2].onclick = () => {
+//     location.href = 'class_home.html'; 
+// }
+// nav_atags[3].onclick = () => {
+//     location.href = 'class_home.html'; 
+// }
+
+// 모두 펼치고 닫기
+$(function (){
+	$(".cla-first span:last-child").click(function (){
+  	$(".cla-contents").toggle();
+  });
+});
+
+const contetnts = document.querySelectorAll('.cla-cont');
+const contetntsList = document.querySelectorAll('.cla-contents');
+
+for(let i =0; i < contetnts.length; i++){
+    contetnts[i].onclick = () =>{
+        if(contetnts[i] == contetntsList[i]){
+            contetntsList[i].style.display = 'block';
+        };
+    };
+};
